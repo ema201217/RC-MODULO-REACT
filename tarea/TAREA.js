4c8a4095f37a15c2911b0f1b2ccf6568a1eb4795
Emanuel Arroyo <ema201217@gmail.com>
2023-01-15 19:32:03 -0300

    clase 4 - CounterApp

---
+++ tarea/TAREA.js
@@ -0,0 +1,3 @@
+// Ir al campus y en el módulo 3 encontrarás el apartado ejercicios. Abrir el documento y realizar el ejercicio Hello world.
+
+// link del ejercicio --> https://docs.google.com/document/d/1yFK09NIwbUug5p0M_q1ESPXH4xaCS9sNqzYEOehxoJc/edit#heading=h.h4v4sgit8k7g
