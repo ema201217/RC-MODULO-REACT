409c47d6f2290b14af532710393ebe79586e907c
Emanuel Arroyo <ema201217@gmail.com>
2023-02-21 04:53:34 -0300

    parte 3 del project (register,login and createProduct)

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,46 @@
+import { useState } from "react";
 import { Button, Col, Form, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { Layout } from "../layouts/Layout";
 
 export const Login = () => {
+  const [formValues, setFormValues] = useState({ email: "", password: "" });
+  const [error, setError] = useState("");
+
+  const handleChange = ({ target }) => {
+    setFormValues({
+      ...formValues,
+      [target.name]: target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!formValues.email || !formValues.password) {
+      setError("Todos los campos son obligatorios");
+      return;
+    }
+    console.log(formValues);
+
+    fetch(`http://localhost:3000/users?email=${formValues.email}`)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.length === 0) {
+          setError("Este email no esta registrado");
+          return;
+        }
+
+        const user = data[0];
+        if (user.password !== formValues.password) {
+          setError("Credenciales invalidas");
+          return;
+        }
+
+        console.log("Usuario ingresado con éxito");
+      });
+    setError("");
+  };
   return (
     <Layout>
       <Row className="my-5">
@@ -10,10 +48,15 @@ export const Login = () => {
           <h1>Login</h1>
         </Col>
         <Col md={{ span: 4, offset: 4 }}>
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Control
+                type="email"
+                name="email"
+                placeholder="Enter email"
+                onChange={handleChange}
+              />
               <Form.Text className="text-muted">
                 Ingresar email con el que te registraste
               </Form.Text>
@@ -21,9 +64,14 @@ export const Login = () => {
 
             <Form.Group className="mb-3" controlId="">
               <Form.Label>Contraseña</Form.Label>
-              <Form.Control type="password" placeholder="" />
+              <Form.Control
+                type="password"
+                name="password"
+                placeholder="Ingresar contraseña"
+                onChange={handleChange}
+              />
             </Form.Group>
-
+            {error && <p className="text-danger">{error}</p>}
             <Button variant="primary" type="submit">
               Iniciar Sesión
             </Button>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,36 +1,61 @@
+import { useState } from "react";
 import { Button, Col, Form, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { Layout } from "../layouts/Layout";
 
 export const Register = () => {
+  const [formValues, setFormValues] = useState({ email: "", password: "" });
+  const [error, setError] = useState("");
+
+  const handleChange = ({ target }) => {
+    setFormValues({
+      ...formValues,
+      [target.name]: target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!formValues.email || !formValues.password) {
+      setError("Todos los campos son obligatorios");
+      return;
+    }
+
+    fetch("http://localhost:3000/users", {
+      method:'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formValues)
+    })
+  };
+
   return (
     <Layout>
       <Row className="my-5">
-      <Col md={12} className="text-center">
+        <Col md={12} className="text-center">
           <h1>Register</h1>
         </Col>
         <Col md={{ span: 4, offset: 4 }}>
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Email</Form.Label>
-              <Form.Control type="email" />
+              <Form.Control type="email" name="email" onChange={handleChange} />
               <Form.Text className="text-muted">
                 Ingresar email para registrarte
               </Form.Text>
             </Form.Group>
-
             <Form.Group className="mb-3">
               <Form.Label>Contraseña</Form.Label>
-              <Form.Control type="password" />
-            </Form.Group>
-
-            <Form.Group className="mb-3" controlId="terminos">
-              <Form.Check
-                type="checkbox"
-                label="Aceptar términos y condiciones"
-                id="terminos"
+              <Form.Control
+                type="password"
+                name="password"
+                onChange={handleChange}
               />
             </Form.Group>
+            {error && <p className="text-danger">{error}</p>}
+
             <Link to="/login" className="link-dark mx-2">
               ¿Ya tienes cuenta? Iniciar sesión
             </Link>
